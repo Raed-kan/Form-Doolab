1c7632ad1bfca8ef1659e356b9adf2f095593484
Raed-kan <raeddesign25@gmail.com>
2026-06-18 12:06:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,7 @@
    مثال:
    const ENDPOINT_URL = "https://script.google.com/macros/s/AKfy.../exec";
    ============================================================ */
-
-const ENDPOINT_URL = "";
+const ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbw5NO7SS4OnWtHMTQbaDR-4Hfo36pOf8_l4Bf5A0ayG3MbpNZJOuF4zmC126-YaA_Py/exec";
 
 /* لا تعدّل تحت هذا السطر إلا إذا تبغى تغيّر السلوك */
 const CONFIG = {
